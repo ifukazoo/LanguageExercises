2efd8cc444a4bb12e9ddfe1b529e9a12c16a87f1
yamanobori_old <yamanobori_old@yahoo.co.jp>
2015-10-10 07:51:19 +0900

    cpp 11

--- cpp/include/functions.h
+++ cpp/include/functions.h
@@ -1,14 +1,22 @@
 #ifndef _FUNCTIONS_H_
 #define _FUNCTIONS_H_
 
+#include <cstdlib>
 #include <cerrno>
 
+#include <algorithm>
 #include <numeric>
 #include <limits>
 
 struct IsDigit : public std::binary_function<bool, char, bool> {
   bool operator() (bool result, char c) {
     return !result ? result : (isdigit(c) > 0);
+  }
+};
+
+struct IsDigitOrPoint : public std::binary_function<bool, char, bool> {
+  bool operator() (bool result, char c) {
+    return !result ? result : (isdigit(c) > 0 || c == '.');
   }
 };
 
@@ -46,6 +54,27 @@ std::pair<bool, Integer> string_to_integer(std::string s)
     return std::make_pair(false, 0);
   }
   return std::make_pair(true, static_cast<Integer>(long_val));
+}
+
+template<typename Float>
+std::pair<bool, Float> str_to_f(std::string s)
+{
+  if (s.empty()) {
+    return std::make_pair(false, 0);
+  }
+
+  char* endptr;
+  errno = 0;
+  double double_val = strtod(s.c_str(), &endptr);
+  if (errno || *endptr != '\0') {
+    return std::make_pair(false, 0);
+  }
+  bool toolarge = double_val >  std::numeric_limits<Float>::max();
+  bool toosmall = double_val < -std::numeric_limits<Float>::max();
+  if (toolarge || toosmall) {
+    return std::make_pair(false, 0);
+  }
+  return std::make_pair(true, static_cast<Float>(double_val));
 }
 
 #endif
